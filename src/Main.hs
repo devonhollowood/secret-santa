57c2be38f46ba67b322d380a1a19b09543c4614f
Devon Hollowood <devonhollowood@gmail.com>
2015-11-30 17:03:00 -0800

    Code Cleanup

--- src/Main.hs
+++ src/Main.hs
@@ -2,11 +2,12 @@ import qualified Network.HaskellNet.SMTP.SSL as SMTP
 import Options.Applicative as Opt
 import Control.Monad.Random
 import Control.Monad (mzero, liftM, when)
-import Data.Csv hiding (Name)
+import qualified Data.Csv as Csv
+import Data.Csv ((.!))
 import qualified Data.ByteString.Lazy as B
 import qualified Data.Vector as V
 import qualified Data.Text.Lazy as T
-import System.Random.Shuffle
+import System.Random.Shuffle (shuffleM)
 import System.Directory (doesFileExist)
 import System.Exit (die)
 import System.IO (hFlush, stdout)
@@ -14,15 +15,16 @@ import Data.List (find)
 import Data.Maybe (maybeToList)
 
 main = do
-    opts <- execParser parseOpts
-    people <- readPeopleFile (peopleFile opts)
-    forbidden_pairs <- maybe
+    opts <- execParser parseOpts -- Get command line options
+    people <- readPeopleFile (peopleFile opts) -- Get list of people
+    forbidden_pairs <- maybe                   -- Get list of forbidden pairs
                       (return [])
                       readForbiddenPairsFile
                       (forbiddenPairsFile opts)
-    maybe_santas <- evalRandIO $ selectSantas people forbidden_pairs
-    case maybe_santas of
-        Just santas -> santaAction opts santas
+    maybe_santas <- evalRandIO $               -- Get Maybe(list of pairings)
+        selectSantas people forbidden_pairs
+    case maybe_santas of                       -- Perform action chosen at
+        Just santas -> santaAction opts santas -- command line if possible
         Nothing -> die "Error: no valid pairings found!"
 
 {- Reads list of people from csv file `filename`.
@@ -30,8 +32,8 @@ main = do
  -}
 readPeopleFile :: FileName -> IO [Person]
 readPeopleFile filename = do
-    csv_data <- B.readFile filename
-    case readCsv csv_data of
+    csv_data <- B.readFile filename   -- Try to read list-of-people file
+    case readCsv csv_data of          -- Make sure we were successful
         Right people -> return people
         Left err -> die err
 
@@ -40,16 +42,16 @@ readPeopleFile filename = do
  -}
 readForbiddenPairsFile :: FileName -> IO [ForbiddenPair]
 readForbiddenPairsFile filename = do
-    csv_data <- B.readFile filename
-    case readCsv csv_data of
+    csv_data <- B.readFile filename -- Try to read forbidden pairs file
+    case readCsv csv_data of        -- Make sure we were successful
         Right pairs -> return pairs
         Left err -> die err
 
 {- Takes raw csv file data, and returns either an error message or a list of
  - `a`s contained in the file data
  -}
-readCsv :: FromRecord a => B.ByteString -> Either String [a]
-readCsv csv_data = V.toList <$> decode NoHeader csv_data
+readCsv :: Csv.FromRecord a => B.ByteString -> Either String [a]
+readCsv csv_data = V.toList <$> Csv.decode Csv.NoHeader csv_data
 
 {- Randomly pairs people with a secret santa, rejecting any sets which contain
  - forbidden pairs
@@ -60,10 +62,9 @@ selectSantas people forbidden =
     liftM                                       -- Do in the Rand g Monad
     (find (not . any is_forbidden) . take 1000) -- Pick first non-forbidden list
     (generatePairs people)                      -- From 1000 shuffled lists
-    where is_forbidden :: (Person, Person) -> Bool
-          is_forbidden (a, b) =
-              name a == name b ||
-              (name a, name b) `elem` forbidden ||
+    where is_forbidden (a, b) =                 -- A person cannot be paired:
+              name a == name b ||                  -- with themselves
+              (name a, name b) `elem` forbidden || -- with a forbidden pairing
               (name b, name a) `elem` forbidden
 
 {- Generates an infinite list of lists of randomly-paired Persons from `people`
@@ -76,57 +77,54 @@ generatePairs people =
 
 {- Action which prints out the assignments, and nothing else
  -}
-testSantas :: Santas -> IO ()
-testSantas = mapM_ (putStrLn . santaLine)
+testSantas :: Action
+testSantas = mapM_ (putStrLn . santaLine) -- Print out line for each pairing
     where santaLine (a, b) =
               concat ["Assigned ", name a, " (", email a,
                       ") their secret santa: ", name b]
 
 {- Action which sends out emails to the Santas in `santas`
  -}
-sendEmails :: Santas -> IO ()
+sendEmails :: Action
 sendEmails santas = SMTP.doSMTPSTARTTLS "smtp.gmail.com" $ \connection -> do
     doubleSendGuard -- Make sure we haven't already sent the emails
-    (from, username) <- authenticate connection -- Connect to server
+    from <- prompt "What is your name? (used in the \"from\" field)"
+    authenticate connection -- Connect to server
     putStrLn "Sending emails..."
-    mapM_ (sendEmail connection from username) santas -- Send the emails
+    mapM_ (sendEmail connection from) santas -- Send the emails
     putStrLn "\t...success!"
     writeFile doubleSendGuardFile "" -- Protect against future double-sends
 
 {- Sends a secret santa email to `gifter`, assigning them `giftee`
  - `connection` is the SMTP connection to use
- - `username` is the username to log in with
+ - `from` is what to use in the "Sender" field
  -}
-sendEmail :: SMTP.SMTPConnection -> Name -> UserName -> (Person, Person)
-             -> IO ()
-sendEmail connection from username (gifter, giftee) =
+sendEmail :: SMTP.SMTPConnection -> Name -> (Person, Person) -> IO ()
+sendEmail connection from (gifter, giftee) =
     putStrLn ("\t...sending to " ++ email gifter ++ "...") >>
-    SMTP.sendPlainTextMail     -- Send email
-    (email gifter)             -- Recipient
-    (username ++ "@gmail.com") -- Sender
-    "Secret Santa (Shhh!)"     -- Subject
-    (T.pack $ unlines          -- Body
+    SMTP.sendPlainTextMail      -- Send email
+    (email gifter)              -- Recipient
+    from                        -- Sender
+    "Secret Santa (Shhh!)"      -- Subject
+    (T.pack $ unlines           -- Body
         ["Hello " ++ name gifter ++ ",",
          "Your secret santa giftee is " ++ name giftee ++ ".",
          "Get them something good!"
         ]
     )
-    connection                 -- SMTP Connection
+    connection                  -- SMTP Connection
 
-{- Prompts for username and password, and authenticates. Returns the username
- - (e.g. "devonhollowood") for the connection, as well as the
+{- Prompts for username and password, then authenticates these credentials
  -}
-authenticate :: SMTP.SMTPConnection -> IO (Name, UserName)
+authenticate :: SMTP.SMTPConnection -> IO ()
 authenticate connection = do
-    from <- prompt "What is your name? (for the \"from\" field)"
     username <- prompt "What is your gmail username?"
     password <- prompt "What is your gmail password?"
     putStrLn "Authenticating..."
     successful <- SMTP.authenticate SMTP.PLAIN username password connection
-    if successful
-        then putStrLn "\t...success!" >> return (from, username)
+    if successful -- Make sure authentication was successful
+        then putStrLn "\t...success!"
         else die "Authentication failed"
-    where prompt msg = putStr (msg ++ " ") >> hFlush stdout >> getLine
 
 {- Guards against accidentally sending the secret santa information twice
  - by checking to see if the file `doubleSendGuardFile` already exists
@@ -142,22 +140,29 @@ doubleSendGuard = do
 doubleSendGuardFile :: FileName
 doubleSendGuardFile = "already-sent.guard"
 
+{- Prompts user for input using `msg`
+ -}
+prompt :: String -> IO String
+prompt msg = putStr (msg ++ " ") >> hFlush stdout >> getLine
+
 {- Type declarations
  -}
 type FileName = String
 type Name = String
 type Email = String
 type UserName = String
-
-type Santas = [(Person, Person)]
 type ForbiddenPair = (Name, Name)
+type Santas = [(Person, Person)]
+type Action = Santas -> IO ()
 
 data Person = Person {
     name :: Name,
     email :: Email
 } deriving Show
 
-instance FromRecord Person where
+{- Allow Persons to be parsed from CSV records
+ -}
+instance Csv.FromRecord Person where
     parseRecord record
         | V.length record == 2 = Person <$> record .! 0 <*> record .! 1
         | otherwise = mzero
@@ -167,7 +172,7 @@ instance FromRecord Person where
 {- Datatype representing command-line options
  -}
 data Options = Options {
-    santaAction :: Santas -> IO (),
+    santaAction :: Action,
     peopleFile :: FileName,
     forbiddenPairsFile :: Maybe FileName
 }
